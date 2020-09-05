f86a517ef3c30d8826c982dc014329266137b733
Daniel Chen <chendaniely@gmail.com>
2020-09-05 16:00:56 -0400

    :tada:

--- src/030-load_songs.py
+++ src/030-load_songs.py
@@ -1,11 +1,13 @@
+import pandas as pd
 import soundfile as sf
 
 from pyprojroot import here
 
 import src.beatsaber as bs
 
-data, samplerate = sf.read(here("./songs/294c/song.egg")) # manually renamed song.egg
+pd.set_option('precision', 20)
 
+data, samplerate = sf.read(here("./songs/294c/song.egg")) # manually renamed song.egg
 
 data
 samplerate
@@ -24,4 +26,36 @@ df_map
 #f
 
 df_map['_events']
-df_map['_events'][0]
+df_map['_events'][0][0]
+
+def list_dict_to_df(df, colname, index_name='_time'):
+    df = pd.DataFrame(df[colname][0])
+    df.index = df[index_name]
+    df = df.drop('_time', axis='columns')
+    df.columns = df.columns.str.replace('_', f"{colname.replace('_', '')}_")
+    return(df)
+
+events_df = list_dict_to_df(df_map, '_events')
+notes_df = list_dict_to_df(df_map, '_notes')
+obstacles_df = list_dict_to_df(df_map, '_obstacles')
+
+df_map_tidy = pd.concat([events_df, notes_df, obstacles_df], axis='columns')
+
+df_map_tidy.shape
+
+df_map_tidy.events_type.value_counts(sort=False, dropna=False).sort_index()
+df_map_tidy.notes_type.value_counts(sort=False, dropna=False).sort_index()
+df_map_tidy.obstacles_type.value_counts(sort=False, dropna=False).sort_index()
+
+
+import matplotlib.pyplot as plt
+
+
+plt.plot(data)
+plt.show()
+
+df_song = pd.DataFrame(data)
+df_song
+
+
+df_map_tidy.index * samplerate
